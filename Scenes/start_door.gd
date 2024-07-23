5ddd7771b176dd8f8db5a045f1fed9329228b2ab
fusion407 <61926486+fusion407@users.noreply.github.com>
2024-07-23 14:14:07 -0500

    made a new scene for the players house, and the door to start game

---
+++ Scenes/start_door.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var playerOnDoor = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if !playerOnDoor:
+		return
+	if event.is_action_pressed("chat"):
+		print("game start?")
+		get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+
+func _on_start_game_area_body_entered(body):
+	if body.has_method("player"):
+		playerOnDoor = true
+		print("Click F to start game.")
+
+
+func _on_start_game_area_body_exited(body):
+	if body.has_method("player"):
+		playerOnDoor = false
+		print("or not.....")
