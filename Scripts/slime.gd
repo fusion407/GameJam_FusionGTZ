0fc5aa7fad64f3fc926adad04709a228cca6199a
Corbin McKay <corbinmckay97@gmail.com>
2024-07-26 16:44:21 -0500

    slime drop changes - test
    testing having multiple different item drops for a monster, with randomized chances.

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -4,10 +4,15 @@ var speed = 100
 var health = 100
 
 var dead = false
-var player_in_area
-var player
+var player_in_area = false
+var player = null
+var randNum
 
 @onready var slime = $slime_collectable
+@onready var bone = $bone_collectable
+
+# load item res variable
+@export var addBoneInv = preload("res://Inventory/items/bone.tres") 
 @export var itemRes: InvItem
 
 func _ready():
@@ -36,6 +41,7 @@ func _on_detection_area_body_entered(body):
 func _on_detection_area_body_exited(body):
 	if body.has_method("player"):
 		player_in_area = false
+		player = null
 
 
 func _on_hitbox_area_entered(area):
@@ -54,24 +60,45 @@ func death():
 	$AnimatedSprite2D.play("death")
 	await get_tree().create_timer(1).timeout
 	
-	# todo: write function to calculate item drop chance
-	drop_slime()
+	# get random number
+	randomize()
+	randNum = randi_range(1, 100)
+	
+	# decide drop based on number
+	if randNum > 85:     # 15% chance to drop bone
+		drop_bone()
+	elif randNum <= 85 && randNum >= 35:     # 50% chance to drop slime
+		drop_slime()     
+	# else - 35% no drops for wizard, get rekt
 	
 	$AnimatedSprite2D.visible = false
 	$hitbox/CollisionShape2D.disabled = true
 	$detection_area/CollisionShape2D.disabled = true
 	
+# drop and collection functions
 func drop_slime():
 	slime.visible = true
 	$slime_collect_area/CollisionShape2D.disabled = false
 	slime_collect()
 	
+func drop_bone():
+	bone.visible = true
+	$slime_collect_area/CollisionShape2D.disabled = false
+	bone_collect()
+	
 func slime_collect():
 	await get_tree().create_timer(1.5).timeout
 	slime.visible = false
-	player.collect(itemRes)
+	if player: 
+		player.collect(itemRes)
 	queue_free()
 
+func bone_collect():
+	await get_tree().create_timer(1.5).timeout
+	bone.visible = false
+	if player: 
+		player.collect(addBoneInv)
+	queue_free()
 
 
 func _on_slime_collect_area_body_entered(body):
