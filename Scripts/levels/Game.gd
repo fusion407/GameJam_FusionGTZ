cece9d161423fb01336a3a947f2856bd2452e9fe
fusion407 <61926486+fusion407@users.noreply.github.com>
2024-07-27 01:22:35 -0500

    organizing scripts and scenes

--- Scripts/levels/Game.gd
+++ Scripts/levels/Game.gd
@@ -8,13 +8,14 @@ func _ready():
 	$Fade_transition/AnimationPlayer.play("fade_out")
 	
 	$Player.game_has_started = true
+	$Player.isAlive = true
 	
 	
 
 
 func _on_game_timer_timeout():
 	print("timer run out. yay")
-	
+	$Player.game_has_started = false
 	$Fade_transition/fade_timer.start()
 	$Fade_transition/AnimationPlayer.play("fade_in")
 	await get_tree().create_timer(5).timeout
--- Scripts/levels/house.gd
+++ Scripts/levels/house.gd
@@ -6,6 +6,10 @@ func _ready():
 	$Fade_transition.show()
 	$Fade_transition/fade_timer.start()
 	$Fade_transition/AnimationPlayer.play("fade_out")
+	
+	$Player.health = 1000
+	$Player.isAlive = true
+	$Player.game_has_started = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
