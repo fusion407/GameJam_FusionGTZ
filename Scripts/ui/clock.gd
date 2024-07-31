2088b7ae504b740f46817af3e79d176be8ab6787
Corbin McKay <corbinmckay97@gmail.com>
2024-07-31 01:19:25 -0500

    adding night lighting effect

--- Scripts/ui/clock.gd
+++ Scripts/ui/clock.gd
@@ -1,62 +1,77 @@
+#-----------------------------------------------------------------------------------------------
+# clock.gd
+# Author: Corbin McKay
+# script to display in game clock during wizards nightly quest from 10pm-6am
+# a signal is received from the game timer to receive the total set time for 
+# the level. Hour intervals are calculated by taking the total time divided by 8
+# current time in the nightly quest is calculated using delta and converted to seconds
+# the hour is updated after elapsed time goes up by each set interval. 
+# even if the level time is changed, the functionality remains the same.
+#-----------------------------------------------------------------------------------------------
+
 extends CanvasLayer
 
-var level_time
-var hour_interval: float
-var time_elapsed: float
-var time_seconds
+var level_time     # time the level will last
+var hour_interval: float     # how many real seconds for each in game hour
+var time_elapsed: float      # elapsed time
+
+# variables to hold and display in game time
 var displayHour
 var displayMin
 var amPM = "PM"
-var inEnvironment: bool
 
-
+# signals to send to lighting scene, to tell which light layer to use
+signal light1
+signal light2
+signal light3
+signal light4
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	inEnvironment = true
+	pass
 	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# gets current time elapsed and calls to check if time is to be updated every frame
 func _process(delta):
 	time_elapsed += delta
-	time_seconds = fmod(time_elapsed, 60)
-	if (level_time - time_seconds) > delta: 
-		updateTime()
-	else:
-		# game time will reset back to zero as transition starts, will make time 10 again otherwise
-		inEnvironment = false
-	
+	updateTime()
 
+# checks for time update and writes the current time to the label
 func updateTime():
-	if inEnvironment:
-		getHour(time_seconds)
-		$Label.text = str(displayHour) + ":00 " + amPM
-	else:
-		$Label.text = "6:00 AM"
-	
+	getHour(time_elapsed)
+	$Label.text = str(displayHour) + ":00 " + amPM
 
+	
+# if elif hell, check when game time passes each hour interval
 func getHour(gameTime):
 	if gameTime < hour_interval:
 		displayHour = 10
+		light1.emit()
 	elif gameTime < (hour_interval * 2):
 		displayHour = 11
+		light2.emit()
 	elif gameTime < (hour_interval * 3):
 		displayHour = 12
 		amPM = "AM"
+		light3.emit()
 	elif gameTime < (hour_interval * 4):
 		displayHour = 1
+		light4.emit()
 	elif gameTime < (hour_interval * 5):
 		displayHour = 2
 	elif gameTime < (hour_interval * 6):
 		displayHour = 3
+		light3.emit()
 	elif gameTime < (hour_interval * 7):
 		displayHour = 4
+		light2.emit()
 	elif gameTime < (hour_interval * 8):
 		displayHour = 5
-	
+		light1.emit()
+	else: 
+		displayHour = 6
 	
 
-
+# runs when game timer signals, passing the wait time (total level time)
 func _on_game_timer_has_time(time):
 	level_time = time
 	hour_interval = level_time / 8
